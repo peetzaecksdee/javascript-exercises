84dc0643d16cd7ec6c746077085703c0f83657ac
peetzaecksdee <peetzaecksdee@hotmail.com>
2022-10-14 21:41:45 +0700

    Did some more

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (number < 0) return "OOPS";
+  let fib = [0, 1];
+  let temp = 0;
+  for (let i = 0; i < number; i++) {
+    temp = fib[0] + fib[1];
+    fib[0] = fib[1];
+    fib[1] = temp;
+  }
+  return fib[0];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+  return obj.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    return oldestAge < currentAge ? currentPerson : oldest
+  })
 };
+
+const getAge = function(birthDate, deathDate) {
+  if (!deathDate) {
+    deathDate = new Date().getFullYear()
+  }
+  return deathDate - birthDate
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
